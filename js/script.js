beee733f5b16ac935ce3aec4059c78ae3319ed08
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-07 01:15:00 +0200

    function add

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 //JS RESET
 console.log('JS OK')
 
-
-  
+// Funzione per controllare se una parola è palindroma
+const checkPalindrome = (word) => {
+  // Rimuovo spazi e converto tutto in minuscolo
+  const cleanedWord = word.toLowerCase().replace(/\s/g, '');
