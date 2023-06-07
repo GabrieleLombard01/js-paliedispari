7d0de27ce9646f7480d4ee583e2340c757892242
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-07 17:28:37 +0200

    js logic add

--- js/pariDispari.js
+++ js/pariDispari.js
@@ -10,3 +10,13 @@ const form = document.getElementById('paridispari_form');
 const numberField = document.getElementById('number');
 const choiceField = document.getElementById('choice');
 const resultElement = document.getElementById('evenOdd-result');
+
+// LOGICA ->
+form.addEventListener('sumbit', function(event){
+    //Blocco l'esecuzione
+    event.preventDefault();
+
+    //Recupero i valori
+    const userNumber = parseInt(numberField.value);
+    const choice = choiceField.value;
+});
