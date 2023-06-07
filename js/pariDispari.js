ebafc3b98e87591700da7805b4f51206386a959e
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-07 17:25:10 +0200

    DOM elements caught

--- js/pariDispari.js
+++ js/pariDispari.js
@@ -5,3 +5,8 @@ console.log('JS OK')
 |||||||||| PARI E DISPARI -> |||||||||
 \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
 
+// RECUPERO elementi dal dom
+const form = document.getElementById('paridispari_form');
+const numberField = document.getElementById('number');
+const choiceField = document.getElementById('choice');
+const resultElement = document.getElementById('evenOdd-result');
